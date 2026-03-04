01e498763ce7b736b8ba2d9bcb7b04dea7265bcf
Martin van Houte <mvanhoute@anwb.nl>
2026-03-04 11:52:22 +0100

    Updated middleware

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -2,7 +2,15 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:5173',
+        'https://sky-3d.statichost.page',
+      ],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
